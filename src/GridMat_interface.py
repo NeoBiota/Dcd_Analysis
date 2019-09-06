0147aebe1c66dccfc060b86f8a47e4f8e96f136b
NeoBiota <p.hammerschick@yahoo.de>
2019-09-06 11:03:41 -0400

    tools with pickled indices

---
+++ src/GridMat_interface.py
@@ -0,0 +1,129 @@
+"""
+Python interface to use GridMat-MD with dcd trajectories
+Uses Robin M. Betz vmd python modules
+Paulina Mueller, 07.2019
+"""
+from __future__ import division
+
+import cPickle as pickle
+import numpy as np
+import os
+import subprocess
+import sys
+import tempfile
+import yaml
+
+from dcd_trajectory import DcdFile
+
+import logging
+logging.basicConfig(filename='log.log', filemode='w', level=logging.INFO)
+
+
+def main():
+    # check for parameter file
+    if len(sys.argv) != 2:
+        print("No parameter file given")
+        sys.exit()
+    # get parameters
+    try:
+        f = open(sys.argv[1])
+    except IOError:
+        print("Couldn't open parameter file")
+        sys.exit()
+    else:
+        with f:
+            params = yaml.load(f, Loader=yaml.FullLoader)
+
+    # assign values
+    try:
+        logging.info("Input dcd file(s) %s" % params['dcd'])
+        logging.info("Input parameter file is %s" % params['perl_param'])
+        # get properties/indices from pickle
+        properties, indices = pickle.load(open(params['pickle']+'.p', "rb"))
+    except KeyError:
+        print("Inputfile(s) missing. Check parameter file. Exiting...")
+        sys.exit(0)
+
+    # setting some defaults
+    if 'resname' not in params:
+        params['resname'] = 'DPPC'
+    if 'atoms' not in params:
+        params['atoms'] = 'P'
+    if 'output' not in params:
+        params['output'] = ["top_pbc", "bottom_pbc"]
+
+    # open outputs:
+    outputs = open_outs(params['output'], params['outname'])
+
+    # loop over trajectories
+    # initialise
+    for fil in params['dcd']:
+        dcd = DcdFile(fil, indices)
+        if not dcd._file:
+            logging.error("no dcd - aborting")
+            sys.exit(1)
+        dcd.parseHeader()
+
+        # do the calculation
+        grid_mat(dcd, outputs, params['perl_param'], properties)
+
+    # clean up
+    for key, outp in outputs.iteritems():
+        outp.close()
+        if os.path.exists("temp_%s.dat" % key):
+            os.remove("temp_%s.dat" % key)
+
+
+def grid_mat(dcd, outputs, param_file, properties):
+
+    # begin looping over frames
+    for i in range(dcd._n_csets):
+
+        logging.info("Currently in frame "+str(i))
+        # open output
+        temp = tempfile.NamedTemporaryFile('w', delete=False)
+        # get unitcell
+        vectors = ",".join(([str(x/10) for x in dcd.nextUnitcell()[0:3]]))
+        print(vectors)
+        # get coordinates and write to tempfile
+        coord = zip(properties, *[iter(np.ravel(dcd.nextCoordset()))]*3)
+        for props, x, y, z in coord:
+            temp.write("%s %s %s  " % props)
+            temp.write("%s %s %s \n" % (x, y, z))
+
+        temp.close()
+
+        # call perl script
+        pipe = subprocess.Popen(["perl", "GridMAT-dcd.pl",
+                                 param_file, temp.name, vectors],
+                                stdout=subprocess.PIPE)
+        # catch output
+        for line in pipe.stdout:
+            logging.info(line)
+
+        os.unlink(temp.name)
+
+        # combine files
+        for key, outp in outputs.iteritems():
+            comb = open("temp_%s.dat" % key, 'r')
+            outp.write(comb.read())
+            comb.close()
+
+
+def open_outs(output, name):
+    file_arr = {}
+    if "top_pbc" in output:
+        file_arr["top_pbc"] = open("%s.top_thick.dat" % name, 'w')
+    if "bottom_pbc" in output:
+        file_arr["bottom_pbc"] = open("%s.bot_thick.dat" % name, 'w')
+    if "average_pbc" in output:
+        file_arr["average_pbc"] = open("%s.avg_thick.dat" % name, 'w')
+    if "top_area" in output:
+        file_arr["top_area"] = open("%s.top_area.dat" % name, 'w')
+    if "bottom_area" in output:
+        file_arr["bottom_area"] = open("%s.bot_area.dat" % name, 'w')
+    return (file_arr)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/lipid_op_fram.py
@@ -0,0 +1,193 @@
+"""
+routine to calculate lipid order parameters from one dcd trajectory files,
+with options for averaging and selecting frames (see yaml file)
+See yaml paramterfile for description of Input and Output options
+Author: Paulina Mueller
+August 2019
+"""
+
+import cPickle as pickle
+import numpy as np
+import yaml
+import sys
+from dcd_analysis import dcd_trajectory
+from operator import itemgetter
+
+
+def main():
+    # check for parameter file
+    if len(sys.argv) != 2:
+        print("No parameter file given")
+        sys.exit()
+    # get parameters
+    try:
+        f = open(sys.argv[1])
+    except IOError:
+        print("Couldn't open parameter file")
+        sys.exit()
+    else:
+        with f:
+            params = yaml.load(f, Loader=yaml.FullLoader)
+
+    dcdfile = params['dcd']
+    frames = params['frames']
+    avg_slice = params['avg_slice']
+    lipid_dict = params['lipids']
+    axis = params['axis']
+    outname = params['outname']
+    output = params['out']
+
+    # load trajectory
+    dcd = dcd_trajectory.DcdFile(dcdfile)
+    if not dcd._file:
+        print('No dcd - aborting...')
+        sys.exit(1)
+    dcd.parseHeader()
+
+    lipid_bonds = pickle.load(open(params['pickle']+'.p', "rb"))
+
+    # call calculation
+    dcd_op_calc(dcd, frames, lipid_bonds, lipid_dict, axis, avg_slice)
+
+    # write output
+    write_output(output, outname, lipid_dict)
+
+
+def dcd_op_calc(dcd, frames, lipid_bonds, lipid_dict, axis, avg_slice):
+    """
+    method to calculate the order parameters from bonds the dcd trajectory
+    of a system specified by  the pdb for lipids specified
+    averaging is done over frames
+    """
+
+    # if frames is a tuple, make a list
+    if type(frames) is tuple:
+        try:
+            frame_list = range(frames[0], frames[1]+frames[2], frames[2])
+            # get last frame if -1
+            if frames[1] == -1:
+                frame_list = range(frames[0], dcd._n_csets, frames[2])
+        except IndexError:
+            print("Frames is not of the right format. Give list or tuple of 3")
+            sys.exit()
+    elif type(frames) is list:
+        frame_list = frames
+    else:
+        print("Frames is not of the right format. Give list or tuple of 3")
+        sys.exit()
+
+    # start looping
+    for lipid, bonds, res_ids in lipid_bonds:
+        # initalising the data container
+        res_num = bonds.shape[1]
+        # ops_per_frame: for every bond a number of frames with res_num entries
+        # so every frame has to be read once per lipid
+        ops_per_frame = np.zeros((bonds.shape[0],
+                                  len(frame_list), res_num))
+        for i, frame in enumerate(frame_list):
+            dcd.find_frame(frame)
+            coor = dcd.nextCoordset()
+            for j, bond in enumerate(bonds):
+                # calc z vectors
+                vz = np.zeros((res_num, 3))
+                vz_norm2 = np.zeros(res_num)
+                # z-axis of molecular frame of reference is vector in direction of the bond
+                vz[:, 0] = itemgetter(bond[:, 0])(coor)[:, 0] - itemgetter(bond[:, 2])(coor)[:, 0]
+                vz[:, 1] = itemgetter(bond[:, 0])(coor)[:, 1] - itemgetter(bond[:, 2])(coor)[:, 1]
+                vz[:, 2] = itemgetter(bond[:, 0])(coor)[:, 2] - itemgetter(bond[:, 2])(coor)[:, 2]
+                # normalise
+                vz_norm2 = vz[:, 0]**2 + vz[:, 1]**2 + vz[:, 2]**2
+                vz[:, 0] = vz[:, 0]/np.sqrt(vz_norm2)
+                vz[:, 1] = vz[:, 1]/np.sqrt(vz_norm2)
+                vz[:, 2] = vz[:, 2]/np.sqrt(vz_norm2)
+
+                # calc x vectors
+                vtemp1 = np.zeros((res_num, 3))
+                vtemp2 = np.zeros((res_num, 3))
+                # first temporary vector
+                vtemp1[:, 0] = itemgetter(bond[:, 1])(coor)[:, 0] - itemgetter(bond[:, 2])(coor)[:, 0]
+                vtemp1[:, 1] = itemgetter(bond[:, 1])(coor)[:, 1] - itemgetter(bond[:, 2])(coor)[:, 1]
+                vtemp1[:, 2] = itemgetter(bond[:, 1])(coor)[:, 2] - itemgetter(bond[:, 2])(coor)[:, 2]
+                # second temporary vector
+                vtemp2[:, 0] = itemgetter(bond[:, 1])(coor)[:, 0] - itemgetter(bond[:, 0])(coor)[:, 0]
+                vtemp2[:, 1] = itemgetter(bond[:, 1])(coor)[:, 1] - itemgetter(bond[:, 0])(coor)[:, 1]
+                vtemp2[:, 2] = itemgetter(bond[:, 1])(coor)[:, 2] - itemgetter(bond[:, 0])(coor)[:, 2]
+                # product them
+                vx = np.cross(vtemp1, vtemp2)
+                # normalise
+                vx_norm2 = np.zeros(res_num)
+                vx_norm2 = vx[:, 0]**2 + vx[:, 1]**2 + vx[:, 2]**2
+                vx[:, 0] = vx[:, 0]/np.sqrt(vx_norm2)
+                vx[:, 1] = vx[:, 1]/np.sqrt(vx_norm2)
+                vx[:, 2] = vx[:, 2]/np.sqrt(vx_norm2)
+
+                # calc y
+                vy = np.cross(vz, vx)
+                # normalise
+                vy_norm2 = np.zeros(res_num)
+                vy_norm2 = vy[:, 0]**2 + vy[:, 1]**2 + vy[:, 2]**2
+                vy[:, 0] = vy[:, 0]/np.sqrt(vy_norm2)
+                vy[:, 1] = vy[:, 1]/np.sqrt(vy_norm2)
+                vy[:, 2] = vy[:, 2]/np.sqrt(vy_norm2)
+
+                # calculate the order parameter
+                ops_per_frame[j][i] = (-1)*(
+                                           (0.6667)*(0.5*(3*(vx[:, axis]**2) - 1)) +
+                                           (0.333)*(0.5*(3*(vy[:, axis])**2) - 1))
+
+        # calculating the average
+
+        # here just averaging all the frames
+        # ops_avg_per_frame: nrow=bonds, ncol=residues
+        if avg_slice == 1:
+            ops_avg_per_frame = np.array([col for col in np.average(ops_per_frame, 1)])
+        # now summing and diving through the accumulated elements on axis 1
+        else:
+            # ops_avg_per_frame: ((number of avgs, residues, bonds))
+            divi = np.tile(np.arange(1, ops_per_frame.shape[1]+1), (res_num, 1))
+            ops_avg_per_frame = np.true_divide(ops_per_frame.cumsum(1), divi.transpose())
+            ops_avg_per_frame = np.array([elem[avg_slice::avg_slice] for elem
+                                         in ops_avg_per_frame])
+
+        # make lipid_dict holding the results of the calculation
+        # dictionary is mutable
+        lipid_dict[lipid] = (ops_avg_per_frame, res_ids)
+
+
+def write_output(output, name, lipid_dict):
+    """
+    method to write calculations to files name+lipid
+    """
+    if "all" in output:
+        # this tests if there are several values or just one average
+        # output will be nrows=residues, ncolumns=bonds, block if avg_slive !=1
+        if len(lipid_dict.values()[0][0].shape) == 2:
+            for lipid, tup in lipid_dict.items():
+                col1 = np.transpose(tup[0])
+                # expand dims so [1, 2, 3] gets [[1], [2], [3]]
+                col2 = np.expand_dims(np.array(list(tup[1]), dtype=int), 1)
+                np.savetxt(name+lipid+'_all.dat', np.hstack([col2, col1]),
+                           fmt='%i'+' %1.4f'*col1.shape[1])
+        else:
+            for lipid, tup in lipid_dict.items():
+                col2 = np.expand_dims(np.array(list(tup[1]), dtype=int), 1)
+                out = open(name+lipid+'_all.dat', 'wb')
+                for i in range(0, tup[0].shape[0]):
+                    col1 = np.transpose(tup[0][i])
+                    np.savetxt(out, np.hstack([col2, col1]),
+                               fmt='%i'+' %1.4f'*col1.shape[1])
+                    out.write('\n \n \n')
+
+    # averaging over residue
+    # output will be nrows=bonds, ncolumns=according to avg_slice
+    if "avg" in output:
+        if len(lipid_dict.values()[0][0].shape) == 2:  # equals avg_slice == 1
+            for lipid, tup in lipid_dict.items():
+                np.savetxt(name+lipid+'_avg.dat', np.average(tup[0], 1), fmt='%1.4f')
+        else:
+            for lipid, tup in lipid_dict.items():
+                np.savetxt(name+lipid+'_avg.dat', np.average(tup[0], 2), fmt='%1.4f')
+
+
+if __name__ == "__main__":
+    main()
